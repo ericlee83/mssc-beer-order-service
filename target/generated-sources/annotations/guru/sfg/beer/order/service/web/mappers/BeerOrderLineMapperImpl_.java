7da4582d9049369e59ade1a2d15e5bc86c1eae9c
ericlee83 <eric-lee-83@hotmail.com>
2020-07-20 14:50:01 +1000

    cleanup pom

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-07-17T16:57:33+1000",
+    date = "2020-07-20T14:21:09+1000",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
